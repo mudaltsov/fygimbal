fbe4062d8cb492da63ea2bc53938d64aef1f4ff9
Micah Elizabeth Scott <micah@misc.name>
2017-04-25 20:17:32 +0000

    Read should be optional

--- fypoke.py
+++ fypoke.py
@@ -6,13 +6,14 @@ import argparse
 import binascii
 
 def hexint(x):
-	return int(x, 16)
+    return int(x, 16)
 
 parser = argparse.ArgumentParser(description='Talk to the Feiyu Tech gimbal')
-parser.add_argument('--port', default='/dev/cu.SLAB_USBtoUART')
+parser.add_argument('--port', default='/dev/ttyUSB0')
 parser.add_argument('--framing', type=hexint, default=fyproto.SHORT_FORM)
 parser.add_argument('--command', type=hexint, default=None)
 parser.add_argument('--target', type=hexint, default=0)
+parser.add_argument('--read', action='store_true')
 parser.add_argument('data', nargs='*')
 args = parser.parse_args()
 
@@ -20,11 +21,12 @@ port = serial.Serial(args.port, baudrate=115200)
 rx = fyproto.PacketReceiver()
 
 if args.command is not None:
-	data = binascii.a2b_hex(''.join(args.data))
-	packet = fyproto.Packet(command=args.command, target=args.target, framing=args.framing, data=data)
-	print(packet)
-	port.write(packet.pack())
+    data = binascii.a2b_hex(''.join(args.data))
+    packet = fyproto.Packet(command=args.command, target=args.target, framing=args.framing, data=data)
+    print(packet)
+    port.write(packet.pack())
 
-while True:
-	for packet in rx.parse(port.read(1)):
-		print(packet)
+if args.read:
+    while True:
+        for packet in rx.parse(port.read(1)):
+            print(packet)
