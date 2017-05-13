699e41d543a28306fde416d63b24dfa8c5af137c
Micah Elizabeth Scott <micah@misc.name>
2017-05-13 00:49:18 +0000

    ARM!

--- firmware/patcher.sh
+++ firmware/patcher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./unpack.py
-r2 -m 0x8004000 -b 16 mcu0.bin -q -w -i mcu0-patches.r2
-r2 -m 0x8004000 -b 16 mcu1.bin -q -w -i mcu1-patches.r2
-r2 -m 0x8004000 -b 16 mcu2.bin -q -w -i mcu2-patches.r2
+r2 -a arm -m 0x8004000 -b 16 mcu0.bin -q -w -i mcu0-patches.r2
+r2 -a arm -m 0x8004000 -b 16 mcu1.bin -q -w -i mcu1-patches.r2
+r2 -a arm -m 0x8004000 -b 16 mcu2.bin -q -w -i mcu2-patches.r2
 ./pack.py
